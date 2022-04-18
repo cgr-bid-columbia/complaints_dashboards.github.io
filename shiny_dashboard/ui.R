88d0b69d4b116927b3abf929d8c88f3d9556d8f0
Yoseph10 <yoseph.ayala@pucp.edu.pe>
2022-04-18 14:48:22 -0500

    update ui using shinydashboard

--- shiny_dashboard/ui.R
+++ shiny_dashboard/ui.R
@@ -1,10 +1,22 @@
 library(shiny)
+library(shinydashboard)
 
-ui <- fluidPage(
-        sidebarLayout(
-                sidebarPanel(
-                        sliderInput("obs", "Number of observations:", min = 10, max = 500, value = 100)
-                ),
-                mainPanel(plotOutput("distPlot"))
+
+
+ui <- dashboardPage(
+        dashboardHeader(title = "Prueba"),
+        dashboardSidebar(),
+        dashboardBody(
+                
+                sidebarLayout(
+                        sidebarPanel(
+                                sliderInput("obs", "Number of observations:", min = 10, max = 500, value = 100)
+                        ),
+                        mainPanel(plotOutput("distPlot"))
+                )
+                
+                
+                
         )
 )
+
