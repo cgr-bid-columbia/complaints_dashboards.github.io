9d41e8f973a4ada8d846314c0d8fdfbcde5e185b
Yoseph10 <yoseph.ayala@pucp.edu.pe>
2022-06-03 12:08:41 -0500

    first testing: google sheets and shinysurvey

---
+++ Survey_CGR.R
@@ -0,0 +1,70 @@
+library(shiny)
+library(shinysurveys)
+library(ggplot2)
+
+library(googledrive)
+library(googlesheets4)
+
+gs4_auth(cache = ".secrets", email = TRUE, use_oob = TRUE)
+
+
+# Define the fields we want to save from the form
+fields <- c("favorite_food")
+
+
+#functions
+
+saveData <- function(data) {
+        # The data must be a dataframe rather than a named vector
+        data <- data %>% as.list() %>% data.frame()
+        # Add the data as a new row
+        sheet_append("1saXE8aAt0ymhsNooGFIJ7Qjd9x3lSv-rmhIQyAkg38Q", data)
+}
+
+loadData <- function() {
+        # Read the data
+        read_sheet("1saXE8aAt0ymhsNooGFIJ7Qjd9x3lSv-rmhIQyAkg38Q")
+}
+
+
+df <- data.frame(question = "What is your favorite food?",
+                 option = "Your Answer",
+                 input_type = "text",
+                 input_id = "favorite_food",
+                 dependence = NA,
+                 dependence_value = NA,
+                 required = F)
+
+ui <- fluidPage(
+        surveyOutput(df = df,
+                     survey_title = "Hello, World!",
+                     survey_description = "Welcome! This is a demo survey showing off the {shinysurveys} package."),
+        
+)
+
+server <- function(input, output, session) {
+        renderSurvey()
+        
+        # Whenever a field is filled, aggregate all form data
+        formData <- reactive({
+                data <- sapply(fields, function(x) input[[x]])
+                data
+        })
+        
+        
+        # When the Submit button is clicked, save the form data
+        observeEvent(input$submit, {
+                saveData(formData() )
+                
+        })
+
+        observeEvent(input$submit, {
+                showModal(modalDialog(
+                        title = "Congrats, you completed your first shinysurvey!",
+                        "You can customize what actions happen when a user finishes a survey using input$submit."
+                ))
+        })
+}
+
+
+shinyApp(ui, server)
